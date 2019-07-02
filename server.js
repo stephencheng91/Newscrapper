3db68a0a2d05361a514e0920996f3b148de587ea
KuanToto <stephencheng91@gmail.com>
2019-07-02 16:49:20 -0700

    heroku update

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 // Routes
 
 // A GET route for scraping the website
