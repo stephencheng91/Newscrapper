ad695e06b8eae0ee0ea5e72e0f11c10e6362e403
KuanToto <stephencheng91@gmail.com>
2019-07-09 16:44:25 -0700

    Create table, need to finish adding notes session

--- public/app.js
+++ public/app.js
@@ -1,24 +1,26 @@
 //Grab the article data from mongo
-$("#start").click(function(event){
+$("#start").click(function (event) {
 
     event.preventDefault();
     $("#tableBody").html();
-    tableRow = $("<tr>")
-    var articleTitle = $("<td>");
-    var articleSummary = $("<td>");
-    var articleLink = $("<td>");
+
+    // var articleTitle = $("<td>");
+    // var articleSummary = $("<td>");
+    // var articleLink = $("<td>");
     var articleNotes = $("<td>");
 
-    $.getJSON("/article", function(data){
+    $.getJSON("/article", function (data) {
         //Only scrape for first five news
-        for (var i = 0; i < 5; i++){
-            articleTitle.append(data[i].title);
-            articleSummary.append(data[i].summary);
-            articleLink.append(data[i].link);
+        for (var i = 0; i < 5; i++) {
+            //Assign ID for each table row from mongo db
+            var tableRow = $("<tr data-id='" + data[i]._id + "'>")
+            tableRow.append("<td>" + data[i].title + "</td>");
+            tableRow.append("<td>" + data[i].summary + "</td>");
+            tableRow.append("<td> <a target='_blank' href='https://www.foxnews.com/" + data[i].link + "'/a>https://www.foxnews.com/"+data[i].link +"</td>");
 
-            tableRow.append(articleTitle, articleSummary, articleLink);    
             $("#tableBody").append(tableRow);
         }
+
     })
 
 })
